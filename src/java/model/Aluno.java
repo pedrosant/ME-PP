03a9416d4fe0fbb84d73226157e7fde5f65b1d58
1271111753 <1271111753@c2a49bqn2qk2.acd.grupotiradentes.com>
2019-11-20 20:19:34 -0300

    Atributos , Métodos e Gets e Sets da Classe Aluno

--- src/java/model/Aluno.java
+++ src/java/model/Aluno.java
@@ -10,5 +10,81 @@ package model;
  * @author 1271111753
  */
 public class Aluno {
+    public String logradouro;
+    public int numero;
+    public String complemento;
+    public String bairro;
+    public String cidade;
+    public String uf;
+    
+    
+    
+    
+    
+    
+    public String cadastrar_endereco(){        
+        return null;        
+    }    
+    
+    public String exibir_endereco(){
+        return null;
+    }
+
+    
+    
+    
+    
+    
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+    
+    
+    
     
 }
